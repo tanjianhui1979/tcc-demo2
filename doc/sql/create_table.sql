256e7f5c0cadd90821909fe8c4a56546464e74cf
tanjianhui <tan_jianhui@163.com>
2018-01-04 10:35:03 +0800

    Init Commit

---
+++ doc/sql/create_table.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `demo`;
+CREATE TABLE `demo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
